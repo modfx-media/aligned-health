12a022b027701ef00679de9284249c3dc9eafd87
hamza <hamzanasim248@gmail.com>
2026-09-16 19:12:04 +0500

    Add Microsoft Clarity tracking script to RootLayout
    - Introduced a new script for Microsoft Clarity to enhance user behavior tracking.
    - The script is loaded after the interactive phase to optimize performance.

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,15 @@ export default function RootLayout({
  gtag('config', 'G-8VEHGC7P8H');
  `}
  </Script>
+ <Script id="microsoft-clarity" strategy="afterInteractive">
+ {`
+ (function(c,l,a,r,i,t,y){
+ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+ t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+ y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+ })(window, document, "clarity", "script", "yj6jmxov5d");
+ `}
+ </Script>
  <Script
  src="https://widgets.leadconnectorhq.com/loader.js"
  data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js"
